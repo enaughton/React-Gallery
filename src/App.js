78de6abe7ef4e6b1ead539a7cc2b75a1d4596e69
enaughton <enaughton5@gmail.com>
2019-09-04 19:36:04 -0400

    ITLIVES!

--- src/App.js
+++ src/App.js
@@ -1,79 +1,37 @@
-import React, { Component } from 'react';
-import {
-  BrowserRouter,
-  Route,
-  Switch
-} from 'react-router-dom';
-import './index.css';
-import SearchForm from './Components/SearchForm';
-import Gallery from './Components/Gallery';
-import Nav from './Components/Nav'
-import apiKey from './config.js';
-
-const key = apiKey
+import React, { Component } from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import "./index.css";
+import SearchForm from "./Components/SearchForm";
+import Gallery from "./Components/Gallery";
+import Nav from "./Components/Nav";
 
 export default class App extends Component {
-
- 
-  
   constructor() {
     super();
     this.state = {
-
       images: [],
-      searchText: ''
-    }
-  } 
-
-  componentDidMount(){
-   this.performSearch(this.state.query);
+      searchText: ""
+    };
   }
 
-
-  performSearch = (query) => {
-
-    fetch(`https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${key}&tags=${query}&per_page=24&format=json&nojsoncallback=1`)
-      .then(response => response.json())
-      .then(responseData => {
-        this.setState({
-          images: responseData.photos.photo })
-      })
-      .catch(error => {
-        console.log('error fetching and parsing data', error)
-      })
-
-
-  }
-
-  render() { 
-    console.log(this.state.images)
+  render() {
+    console.log(this.state.images);
     return (
-      <BrowserRouter>   
-      <div className="container">
-        <div className="main-header">
-          
+      <BrowserRouter>
+        <div className="container">
+          <div className="main-header">
             <h1 className="main-title">FLICKR API SEARCH</h1>
             <SearchForm />
-            <Nav />  
-          </div>   
-          
-        <div className="photo-container">
-          <Gallery data={this.state.images} />
+            <Nav />
+          </div>
+
+          <div className="photo-container">
+            <Switch>
+              <Route path="/:query" component={Gallery} />
+            </Switch>
+          </div>
         </div>
-      </div>
-      
-
-
-      <Switch>
-      <Route path="/:name" render={() => this.performSearch(this.state.searchText)} />
-      <Route path="/Cats" render={() => this.performSearch('cats')} />
-      <Route path="/Dogs" render={() => this.performSearch('dog')}  />
-      <Route path="/Computers" render={() => this.performSearch('computers')}/>
-      </Switch>
-      
-          
       </BrowserRouter>
-     
     );
   }
 }
--- src/Components/Gallery.js
+++ src/Components/Gallery.js
@@ -1,28 +1,37 @@
-import React from 'react';
-import Image from './Image';
+import React from "react";
+import Image from "./Image";
+import apiKey from "../config.js";
 
+const key = apiKey;
 
-const Gallery = props => { 
-
-  const results = props.data
-  
-  
-
-  let images = results.map(image => 
-
-    
-    <Image url={`https://farm${image.farm}.staticflickr.com/${image.server}/${image.id}_${image.secret}.jpg`} key={image.id} />
-  
-    
+const Gallery = ({ match }) => {
+  const query = (match && match.params && match.params.query) || "guitar";
+  const [images, setImages] = React.useState([]);
+  React.useEffect(() => {
+    fetch(
+      `https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${key}&tags=${query}&per_page=24&format=json&nojsoncallback=1`
     )
-  
-  
-  return(
-    
+      .then(response => response.json())
+      .then(responseData => {
+        setImages(responseData.photos.photo);
+      })
+      .catch(error => {
+        console.log("error fetching and parsing data", error);
+      });
+  }, [query]);
+
+  console.log(images);
+
+  return (
     <ul className="gif-list">
-      { images }
-    </ul> 
+      {images.map(image => (
+        <Image
+          url={`https://farm${image.farm}.staticflickr.com/${image.server}/${image.id}_${image.secret}.jpg`}
+          key={image.id}
+        />
+      ))}
+    </ul>
   );
-}
+};
 
 export default Gallery;
